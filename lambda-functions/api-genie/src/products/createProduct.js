5d4115109c5542582e7534b8b5a53f8d9659b1ff
“ridmachathu” <ridmachathu90@gmail.com@vintritech.com>
2023-06-25 17:18:40 -0700

    Added lambda functions

---
+++ lambda-functions/api-genie/src/products/createProduct.js
@@ -0,0 +1,55 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+const { v4: uuid } = require('uuid');
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Get id and name from the body of the request
+    const body = JSON.parse(event.body);
+
+    const payload = {
+        id: uuid(),
+        name : body.name,
+        description: body.description,
+        createDateTime: Date.now()
+    }
+
+    let response = {};
+
+    try {
+        const params = {
+            TableName: tableName,
+            Item: payload
+        };
+
+        const result = await docClient.put(params).promise();
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(payload)
+        };
+    } catch (ResourceNotFoundException) {
+        response = {
+            statusCode: 404,
+            body: "Unable to call DynamoDB. Table resource not found."
+        };
+    }
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
---
+++ lambda-functions/api-genie/src/users/loginUser.js
@@ -0,0 +1,64 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+const passwordHash = require('password-hash');
+const { v4: uuid } = require('uuid');
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Get id and name from the body of the request
+    const body = JSON.parse(event.body);
+
+    response = {
+        statusCode: 200,
+        body: JSON.stringify({ "this": "working" })
+    };
+
+    // const payload = {
+    //     id: uuid(),
+    //     email: body.email,
+    //     nickname: body.nickname,
+    //     password: hashedPassword,
+    //     createDateTime: Date.now()
+    // }
+    // // Creates a new item, or replaces an old item with a new item
+    // // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    // let response = {};
+
+    // try {
+    //     const params = {
+    //         TableName: tableName,
+    //         Item: payload
+    //     };
+
+    //     const result = await docClient.put(params).promise();
+    //     delete payload.password;
+
+    //     response = {
+    //         statusCode: 200,
+    //         body: JSON.stringify(payload)
+    //     };
+    // } catch (ResourceNotFoundException) {
+    //     response = {
+    //         statusCode: 404,
+    //         body: "Unable to call DynamoDB. Table resource not found."
+    //     };
+    // }
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
