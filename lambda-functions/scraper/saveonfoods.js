839969d096217f4e9d246700809b516aa2c7ccae
ridma <ridmachathu90@gmail.com>
2023-07-24 20:40:20 -0700

    added saveonfood more links

--- lambda-functions/scraper/saveonfoods.js
+++ lambda-functions/scraper/saveonfoods.js
@@ -92,6 +92,161 @@ const weblinks = [
       "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-fruit-id-30682?page=3&skip=60",
       "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-fruit-id-30682?page=4&skip=90"
     ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type": "fruits vegetables",
+    "category": "fresh vegetables",
+    "links": [
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-vegetables-id-30694",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-vegetables-id-30694?page=2&skip=30",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-vegetables-id-30694?page=3&skip=60",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/fruits-%26-vegetables/fresh-vegetables-id-30694?page=4&skip=90"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type": "meat",
+    "category": "bacon sausages hot dogs",
+    "links": [
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/deli-%26-ready-made-meals/meat-id-30727",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/deli-%26-ready-made-meals/meat-id-30727?page=2&skip=30",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/deli-%26-ready-made-meals/meat-id-30727?page=3&skip=60"
+      
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type":"dairy and eggs",
+    "category":"milk and cream",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=2&skip=30",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=3&skip=60",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=3&skip=90"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type":"dairy and eggs",
+    "category":"egg and egg substitutes",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/eggs-%26-substitutes-id-30919"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type":"dairy and eggs",
+    "category":"milk and cream",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=2&skip=30",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=3&skip=60",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dairy-%26-eggs/milk-%26-creams-id-30930?page=3&skip=90"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type":"bakery",
+    "category":"Bagels Buns Rolls",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/bakery/bagels-%26-english-muffins-id-30847",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/bakery/bagels-%26-english-muffins-id-30847?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "food",
+    "type":"bakery",
+    "category":"breakfast",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/pantry/breakfast-id-30481",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/pantry/breakfast-id-30481?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "baby",
+    "type":"diapers and potty training",
+    "category":"diapers",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/baby-care/diapers-id-31086",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/baby-care/diapers-id-31086?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "baby",
+    "type":"baby care",
+    "category":"baby food",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/baby-care/baby-food-id-31077",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/baby-care/baby-food-id-31077?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "home and kitchen",
+    "type":"kitchen",
+    "category":"cookware",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/kitchen-%26-dining/cookware-id-31355",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/kitchen-%26-dining/cookware-id-31355?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "home and kitchen",
+    "type":"appliances",
+    "category":"air conditioners and fans",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/appliances/heating-%26-cooling-appliances-id-31313"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "pet food and supplies",
+    "type":"dogs",
+    "category":"dog dry food",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dog/dry-dog-food-id-31296",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/dog/dry-dog-food-id-31296?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "pet food and supplies",
+    "type":"cats",
+    "category":"cat dry food",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/cat/dry-cat-food-id-31290"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "health and beauty",
+    "type":"health care",
+    "category":"cough and cold",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/health-%26-beauty/medicine-%26-health-id-31239",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/health-%26-beauty/medicine-%26-health-id-31239?page=2&skip=30"
+    ]
+  },
+  {
+    "store": "saveonfoods",
+    "class": "health and beauty",
+    "type":"bath and body",
+    "category":"bathing accessories",
+    "links":[
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/personal-care/bath-%26-body-care-id-31118",
+      "https://www.saveonfoods.com/sm/pickup/rsid/1982/categories/personal-care/bath-%26-body-care-id-31118?page=2&skip=30"
+    ]
   }
 ];
 
